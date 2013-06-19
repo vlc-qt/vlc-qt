775944e9a60aa4f6f7b7cbcf10c3648ab1aac6ec
Tadej Novak <tadej@tano.si>
2013-06-19 16:45:11 +0200

    Fix libvlc detection

--- cmake/FindLIBVLC.cmake
+++ cmake/FindLIBVLC.cmake
@@ -32,53 +32,61 @@
 #Put here path to custom location
 #example: /home/user/vlc/include etc..
 FIND_PATH(LIBVLC_INCLUDE_DIR vlc/vlc.h
-    "$ENV{LIBVLC_INCLUDE_PATH}"
+  HINTS "$ENV{LIBVLC_INCLUDE_PATH}"
+  PATHS
+    #Mac OS and Contribs
+    "${CMAKE_CURRENT_SOURCE_DIR}/contribs/include"
+    "${CMAKE_CURRENT_SOURCE_DIR}/contribs/include/vlc"
+    # Env
     "$ENV{LIB_DIR}/include"
     "$ENV{LIB_DIR}/include/vlc"
+    #
     "/usr/include"
     "/usr/include/vlc"
     "/usr/local/include"
     "/usr/local/include/vlc"
     #mingw
     c:/msys/local/include
-    NO_DEFAULT_PATH
-)
-FIND_PATH(LIBVLC_INCLUDE_DIR vlc.h)
+  )
+FIND_PATH(LIBVLC_INCLUDE_DIR PATHS "${CMAKE_INCLUDE_PATH}/vlc" NAMES vlc.h)
 
 #Put here path to custom location
 #example: /home/user/vlc/lib etc..
-FIND_LIBRARY(LIBVLC_LIBRARY NAMES vlc libvlc PATHS
-    "$ENV{LIBVLC_LIBRARY_PATH}"
+FIND_LIBRARY(LIBVLC_LIBRARY NAMES vlc libvlc
+  HINTS "$ENV{LIBVLC_LIBRARY_PATH}"
+  PATHS
     "$ENV{LIB_DIR}/lib"
+    #Mac OS
+    "${CMAKE_CURRENT_SOURCE_DIR}/contribs/lib"
+    "${CMAKE_CURRENT_SOURCE_DIR}/contribs/plugins"
     #mingw
     c:/msys/local/lib
-    NO_DEFAULT_PATH
-)
-
-#Put here path to custom location
-#example: /home/user/vlc/lib etc..
-FIND_LIBRARY(LIBVLC_CORE_LIBRARY NAMES vlccore libvlccore PATHS
-    "$ENV{LIBVLC_LIBRARY_PATH}"
-    "$ENV{LIB_DIR}/lib"
-    #mingw
-    c:/msys/local/lib
-    NO_DEFAULT_PATH
-)
-
+  )
 FIND_LIBRARY(LIBVLC_LIBRARY NAMES vlc libvlc)
+FIND_LIBRARY(LIBVLCCORE_LIBRARY NAMES vlccore libvlccore
+  HINTS "$ENV{LIBVLC_LIBRARY_PATH}"
+  PATHS
+    "$ENV{LIB_DIR}/lib"
+    #Mac OS
+    "${CMAKE_CURRENT_SOURCE_DIR}/contribs/lib"
+    "${CMAKE_CURRENT_SOURCE_DIR}/contribs/plugins"
+    #mingw
+    c:/msys/local/lib
+  )
+FIND_LIBRARY(LIBVLCCORE_LIBRARY NAMES vlccore libvlccore)
 
-IF (LIBVLC_INCLUDE_DIR AND LIBVLC_LIBRARY AND LIBVLC_CORE_LIBRARY)
-    SET(LIBVLC_FOUND TRUE)
-ENDIF (LIBVLC_INCLUDE_DIR AND LIBVLC_LIBRARY AND LIBVLC_CORE_LIBRARY)
+IF (LIBVLC_INCLUDE_DIR AND LIBVLC_LIBRARY AND LIBVLCCORE_LIBRARY)
+   SET(LIBVLC_FOUND TRUE)
+ENDIF (LIBVLC_INCLUDE_DIR AND LIBVLC_LIBRARY AND LIBVLCCORE_LIBRARY)
 
 IF (LIBVLC_FOUND)
-    IF (NOT LIBVLC_FIND_QUIETLY)
-        MESSAGE(STATUS "Found LibVLC include-dir path: ${LIBVLC_INCLUDE_DIR}")
-        MESSAGE(STATUS "Found LibVLC library path: ${LIBVLC_LIBRARY}")
-        MESSAGE(STATUS "Found LibVLC core library path: ${LIBVLC_CORE_LIBRARY}")
-    ENDIF (NOT LIBVLC_FIND_QUIETLY)
+   IF (NOT LIBVLC_FIND_QUIETLY)
+      MESSAGE(STATUS "Found LibVLC include-dir path: ${LIBVLC_INCLUDE_DIR}")
+      MESSAGE(STATUS "Found LibVLC library path:${LIBVLC_LIBRARY}")
+      MESSAGE(STATUS "Found LibVLCcore library path:${LIBVLCCORE_LIBRARY}")
+   ENDIF (NOT LIBVLC_FIND_QUIETLY)
 ELSE (LIBVLC_FOUND)
-    IF (LIBVLC_FIND_REQUIRED)
-        MESSAGE(FATAL_ERROR "Could not find LibVLC")
-    ENDIF (LIBVLC_FIND_REQUIRED)
+   IF (LIBVLC_FIND_REQUIRED)
+      MESSAGE(FATAL_ERROR "Could not find LibVLC")
+   ENDIF (LIBVLC_FIND_REQUIRED)
 ENDIF (LIBVLC_FOUND)
