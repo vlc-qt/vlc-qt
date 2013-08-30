bb89bcde4142b8a5ebb45d6c411abd42c6404a4c
Tadej Novak <tadej@tano.si>
2013-08-30 20:47:43 +0200

    Update Demo Player example

--- examples/demo-player/src/DemoPlayer.cpp
+++ examples/demo-player/src/DemoPlayer.cpp
@@ -1,23 +1,23 @@
 /****************************************************************************
 * VLC-Qt Demo Player
-* Copyright (C) 2012 Tadej Novak <tadej@tano.si>
+* Copyright (C) 2013 Tadej Novak <tadej@tano.si>
 *
 * This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
+* it under the terms of the GNU Lesser General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 *
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
+* GNU Lesser General Public License for more details.
 *
-* You should have received a copy of the GNU General Public License
+* You should have received a copy of the GNU Lesser General Public License
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *****************************************************************************/
 
-#include <QtGui/QFileDialog>
-#include <QtGui/QInputDialog>
+#include <QFileDialog>
+#include <QInputDialog>
 
 #include <vlc-qt/Common.h>
 #include <vlc-qt/Instance.h>
--- examples/demo-player/src/main.cpp
+++ examples/demo-player/src/main.cpp
@@ -1,24 +1,26 @@
 /****************************************************************************
 * VLC-Qt Demo Player
-* Copyright (C) 2012 Tadej Novak <tadej@tano.si>
+* Copyright (C) 2013 Tadej Novak <tadej@tano.si>
 *
 * This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
+* it under the terms of the GNU Lesser General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 *
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
+* GNU Lesser General Public License for more details.
 *
-* You should have received a copy of the GNU General Public License
+* You should have received a copy of the GNU Lesser General Public License
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *****************************************************************************/
 
+
 #include <QtCore/QCoreApplication>
 #include <QtCore/QTextCodec>
-#include <QtGui/QApplication>
+
+#include <QApplication>
 
 #include "DemoPlayer.h"
 
@@ -27,7 +29,9 @@ int main(int argc, char *argv[])
     QCoreApplication::setApplicationName("VLC-Qt Demo Player");
     QCoreApplication::setAttribute(Qt::AA_X11InitThreads);
 
+#if QT_VERSION < 0x050000
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+#endif
 
     QApplication app(argc, argv);
 
