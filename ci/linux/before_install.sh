2a03cf64340c32a5f59ec95db66720fb13079337
Tadej Novak <tadej@tano.si>
2015-04-14 22:49:56 +0200

    [CI] Really do not output extraction verbosely

--- ci/linux/before_install.sh
+++ ci/linux/before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ev
 
 # Prepare build environment
 mkdir dependencies
--- ci/linux/install.sh
+++ ci/linux/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ev
 
 # Install dependencies from APT
 sudo apt-get update -qq
@@ -12,7 +12,7 @@ pushd dependencies
 # Install CMake 3.2.1
 wget http://www.cmake.org/files/v3.2/cmake-3.2.1-Linux-x86_64.tar.gz
 
-tar xvf cmake-3.2.1-Linux-x86_64.tar.gz
+tar xf cmake-3.2.1-Linux-x86_64.tar.gz
 
 # Install Qt 5.4.1
 wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.1-0qt5_essentials.7z
@@ -20,8 +20,8 @@ wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54
 wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.1-0icu_53_1_ubuntu_11_10_64.7z
 wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.1-0icu_53_1_ubuntu_11_10_64.7z.sha1
 
-7z x 5.4.1-0qt5_essentials.7z
-7z x 5.4.1-0icu_53_1_ubuntu_11_10_64.7z
+7z x 5.4.1-0qt5_essentials.7z > /dev/null
+7z x 5.4.1-0icu_53_1_ubuntu_11_10_64.7z > /dev/null
 
 export PATH=$PWD/cmake-3.2.1-Linux-x86_64/bin/:$PWD/5.4/gcc_64/bin/:$PATH
 
