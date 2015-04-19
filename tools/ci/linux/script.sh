e050d8bdc00d8232bcff5c7ef7257456369e89d0
Tadej Novak <tadej@tano.si>
2015-04-19 13:24:31 +0200

    [ci] Fix missing c++11 support

--- tools/ci/linux/script.sh
+++ tools/ci/linux/script.sh
@@ -3,12 +3,12 @@ set -ev
 
 # Configure and build with Qt5
 pushd build-qt5
-cmake .. -DQT_VERSION=5 -DBUILD_TESTS=ON -DLIBVLC_VERSION=0x020000
+cmake .. -DCI=ON -DQT_VERSION=5 -DBUILD_TESTS=ON -DLIBVLC_VERSION=0x020000
 make
 popd
 
 # Configure and build with Qt4
 pushd build-qt4
-cmake .. -DQT_VERSION=4 -DBUILD_TESTS=ON -DLIBVLC_VERSION=0x020000
+cmake .. -DCI=ON -DQT_VERSION=4 -DBUILD_TESTS=ON -DLIBVLC_VERSION=0x020000
 make
 popd
