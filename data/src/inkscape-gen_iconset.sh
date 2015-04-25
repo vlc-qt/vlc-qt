0c61df39892eea2eeca849b72c73a6d93427665f
Tadej Novak <tadej@tano.si>
2015-04-25 21:19:18 +0200

    Regenerate icons and add OS X iconset

---
+++ data/src/inkscape-gen_iconset.sh
@@ -0,0 +1,14 @@
+inkscape --export-png ../../platform/osx/Project.iconset/icon_16x16.png -w 16 vlc-qt.svg
+inkscape --export-png ../../platform/osx/Project.iconset/icon_16x16@2x.png -w 32 vlc-qt.svg
+
+inkscape --export-png ../../platform/osx/Project.iconset/icon_32x32.png -w 32 vlc-qt.svg
+inkscape --export-png ../../platform/osx/Project.iconset/icon_32x32@2x.png -w 64 vlc-qt.svg
+
+inkscape --export-png ../../platform/osx/Project.iconset/icon_128x128.png -w 128 vlc-qt.svg
+inkscape --export-png ../../platform/osx/Project.iconset/icon_128x128@2x.png -w 256 vlc-qt.svg
+
+inkscape --export-png ../../platform/osx/Project.iconset/icon_256x256.png -w 256 vlc-qt.svg
+inkscape --export-png ../../platform/osx/Project.iconset/icon_256x256@2x.png -w 512 vlc-qt.svg
+
+inkscape --export-png ../../platform/osx/Project.iconset/icon_512x512.png -w 512 vlc-qt.svg
+inkscape --export-png ../../platform/osx/Project.iconset/icon_512x512@2x.png -w 1024 vlc-qt.svg
---
+++ data/src/inkscape-gen_vlc-qt.sh
@@ -0,0 +1,20 @@
+inkscape --export-png ../icons/16x16/vlc-qt.png -w 16 vlc-qt.svg
+inkscape --export-png ../icons/16x16/vlc-qt@2x.png -w 32 vlc-qt.svg
+
+inkscape --export-png ../icons/24x24/vlc-qt.png -w 24 vlc-qt.svg
+inkscape --export-png ../icons/24x24/vlc-qt@2x.png -w 48 vlc-qt.svg
+
+inkscape --export-png ../icons/32x32/vlc-qt.png -w 32 vlc-qt.svg
+inkscape --export-png ../icons/32x32/vlc-qt@2x.png -w 64 vlc-qt.svg
+
+inkscape --export-png ../icons/48x48/vlc-qt.png -w 48 vlc-qt.svg
+inkscape --export-png ../icons/48x48/vlc-qt@2x.png -w 96 vlc-qt.svg
+
+inkscape --export-png ../icons/64x64/vlc-qt.png -w 64 vlc-qt.svg
+inkscape --export-png ../icons/64x64/vlc-qt@2x.png -w 128 vlc-qt.svg
+
+inkscape --export-png ../icons/128x128/vlc-qt.png -w 128 vlc-qt.svg
+inkscape --export-png ../icons/128x128/vlc-qt@2x.png -w 256 vlc-qt.svg
+
+inkscape --export-png ../icons/256x256/vlc-qt.png -w 256 vlc-qt.svg
+inkscape --export-png ../icons/256x256/vlc-qt@2x.png -w 512 vlc-qt.svg
