11d48f24fd5c0fa618fee1be52c2bf9404a13a10
Tadej Novak <tadej@tano.si>
2015-06-04 18:30:40 +0200

    [ci] Update OS X dependencies URLs

--- tools/ci/osx/install.sh
+++ tools/ci/osx/install.sh
@@ -9,7 +9,7 @@ brew install qt5 p7zip
 pushd dependencies
 
 # Install patched VLC 2.2.1
-wget http://download.tano.si/dependencies/vlc/vlc-2.2.1-osx_rpath.7z
+curl -LO "https://googledrive.com/host/0B7I7Zg8w-HxRfmxSMmdhM3dTMGJnSklRX1c4ZkxmekJkcFplYnRLTU80R3Znd1VITkNueGs/vlc-2.2.1-osx_rpath.7z"
 7z x vlc-2.2.1-osx_rpath.7z > /dev/null
 
 export PATH=/usr/local/opt/qt5/bin:$PATH
