a59a995fb8769c5ce60ac51a87bc437d5df5b011
Tadej Novak <tadej@tano.si>
2016-05-21 13:06:59 +0200

    [ci] Bump VLC versions

--- scripts/ci/osx/install.sh
+++ scripts/ci/osx/install.sh
@@ -8,9 +8,9 @@ brew install qt5 p7zip ninja lcov
 # Install other dependencies
 pushd dependencies
 
-# Install VLC 2.2.2
-curl -LO http://download.videolan.org/vlc/2.2.2/macosx/vlc-2.2.2.dmg
-hdiutil attach vlc-2.2.2.dmg
+# Install VLC 2.2.3
+curl -LO http://download.videolan.org/vlc/2.2.3/macosx/vlc-2.2.3.dmg
+hdiutil attach vlc-2.2.3.dmg
 
 export PATH=/usr/local/opt/qt5/bin:$PATH
 
