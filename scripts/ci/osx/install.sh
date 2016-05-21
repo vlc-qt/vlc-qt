f82ae5ac499d39bc9c688f9eef9ed94d5c6c1437
Tadej Novak <tadej@tano.si>
2016-05-21 13:14:35 +0200

    [ci] Fix OS X VLC version

--- scripts/ci/osx/install.sh
+++ scripts/ci/osx/install.sh
@@ -8,9 +8,11 @@ brew install qt5 p7zip ninja lcov
 # Install other dependencies
 pushd dependencies
 
-# Install VLC 2.2.3
-curl -LO http://download.videolan.org/vlc/2.2.3/macosx/vlc-2.2.3.dmg
-hdiutil attach vlc-2.2.3.dmg
+export VLC_VERSION=2.2.3
+
+# Install VLC
+curl -LO http://download.videolan.org/vlc/${VLC_VERSION}/macosx/vlc-${VLC_VERSION}.dmg
+hdiutil attach vlc-${VLC_VERSION}.dmg
 
 export PATH=/usr/local/opt/qt5/bin:$PATH
 
--- scripts/ci/osx/script.sh
+++ scripts/ci/osx/script.sh
@@ -5,9 +5,9 @@ set -ev
 pushd build
 cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../install/ \
 	-DWITH_HOMEBREW=ON -DCOVERAGE=ON \
-	-DLIBVLC_INCLUDE_DIR=/Volumes/vlc-2.2.2/VLC.app/Contents/MacOS/include \
-	-DLIBVLC_LIBRARY=/Volumes/vlc-2.2.2/VLC.app/Contents/MacOS/lib/libvlc.dylib \
-	-DLIBVLCCORE_LIBRARY=/Volumes/vlc-2.2.2/VLC.app/Contents/MacOS/lib/libvlccore.dylib
+	-DLIBVLC_INCLUDE_DIR=/Volumes/vlc-${VLC_VERSION}/VLC.app/Contents/MacOS/include \
+	-DLIBVLC_LIBRARY=/Volumes/vlc-${VLC_VERSION}/VLC.app/Contents/MacOS/lib/libvlc.dylib \
+	-DLIBVLCCORE_LIBRARY=/Volumes/vlc-${VLC_VERSION}/VLC.app/Contents/MacOS/lib/libvlccore.dylib
 ninja prepare
 cmake ..
 ninja -v
@@ -19,9 +19,9 @@ popd
 pushd build-static
 cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../install/ \
     -DSTATIC=ON -DWITH_HOMEBREW=ON \
-	-DLIBVLC_INCLUDE_DIR=/Volumes/vlc-2.2.2/VLC.app/Contents/MacOS/include \
-	-DLIBVLC_LIBRARY=/Volumes/vlc-2.2.2/VLC.app/Contents/MacOS/lib/libvlc.dylib \
-	-DLIBVLCCORE_LIBRARY=/Volumes/vlc-2.2.2/VLC.app/Contents/MacOS/lib/libvlccore.dylib
+	-DLIBVLC_INCLUDE_DIR=/Volumes/vlc-${VLC_VERSION}/VLC.app/Contents/MacOS/include \
+	-DLIBVLC_LIBRARY=/Volumes/vlc-${VLC_VERSION}/VLC.app/Contents/MacOS/lib/libvlc.dylib \
+	-DLIBVLCCORE_LIBRARY=/Volumes/vlc-${VLC_VERSION}/VLC.app/Contents/MacOS/lib/libvlccore.dylib
 ninja prepare
 cmake ..
 ninja -v
