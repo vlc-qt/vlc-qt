bed2938beb107061767655824224c8bc42cc884d
Andry <Airmanhip@gmail.com>
2021-02-11 19:04:34 +0300

    fork and update in new repo

--- scripts/win/vlc_symbols_win32.bat
+++
@@ -1,8 +0,0 @@
-echo EXPORTS > libvlc.def
-for /f "usebackq tokens=4,* delims=_ " %%i in (`dumpbin /exports "libvlc.dll"`) do if %%i==libvlc echo %%i_%%j >> libvlc.def
-lib /MACHINE:x86 /DEF:"libvlc.def" /OUT:"sdk\lib\libvlc.lib"
-
-echo EXPORTS > libvlccore.def
-for /f "usebackq tokens=4,* delims=_ " %%i in (`dumpbin /exports "libvlccore.dll"`) do if %%i==vlc echo %%i_%%j >> libvlccore.def
-for /f "usebackq tokens=4,* delims=_ " %%i in (`dumpbin /exports "libvlccore.dll"`) do if %%i==var echo %%i_%%j >> libvlccore.def
-lib /MACHINE:x86 /DEF:"libvlccore.def" /OUT:"sdk\lib\libvlccore.lib"
